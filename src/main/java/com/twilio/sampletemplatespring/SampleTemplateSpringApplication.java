c8b9a459c51a409bbce85df8abefae829b56c431
Martin Mena <mmena@stackbuilders.com>
2020-03-24 15:15:27 -0500

    Initial commit - Spring Boot scaffolding

---
+++ src/main/java/com/twilio/sampletemplatespring/SampleTemplateSpringApplication.java
@@ -0,0 +1,13 @@
+package com.twilio.sampletemplatespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTemplateSpringApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SampleTemplateSpringApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/twilio/sampletemplatespring/SampleTemplateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twilio.sampletemplatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTemplateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

90d08c2d593a3d1c56bf93be89244abebf7245dd
Alejandro Vivanco <avivanco@twilio.com>
2021-11-09 11:09:09 -0500

    [DEVEDSB-98] Fix package in error controller

--- src/main/java/com/twilio/sampletemplatespring/controllers/ErrorController.java
+++ src/main/java/com/twilio/sampletemplatespring/controllers/ErrorController.java
@@ -1,4 +1,4 @@
-package com.twilio.sampletemplatespring;
+package com.twilio.sampletemplatespring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
